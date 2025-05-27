d8bea2e44ecb6e42d9e8609484e41e83fcce5685
fozberry <matt.fosberry@outlook.com>
2025-05-27 17:33:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const http = require('http'); // Node.js built-in module for HTTP
+// index.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const hostname = '0.0.0.0'; // Listen on all available network interfaces
-const port = process.env.PORT || 3000; // Use port provided by hosting, or 3000
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from a Super Simple Node.js App!\n');
+app.get('/', (req, res) => {
+  res.send('Hello from Express.js and Node.js!'); 
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://<span class="math-inline">\{hostname\}\:</span>{port}/`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
